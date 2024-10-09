66d700edc24b94d8407197b78303ca6cb2c7a86b
cheverecachaimachochidoche <ramon_tacv@hotmail.com>
2024-10-08 18:35:26 -0600

    Tercer commit

--- entreact1/src/componentes/navbar.js
+++ entreact1/src/componentes/navbar.js
@@ -5,10 +5,18 @@ const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light fixed-top custom-navbar-bg">
       <a className="navbar-brand" href="#">
-      <img src="/logo.jpeg" alt="Logo" className="logo d-inline-block align-top" />
-      
-      Tu acuario Online!      </a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <img src="/logo.jpeg" alt="Logo" className="logo d-inline-block align-top" />
+        Tu acuario Online!
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
